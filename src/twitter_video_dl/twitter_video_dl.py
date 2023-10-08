1b935ee19f34c12c888c856c6d800c3f2cc30ac0
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-10-09 00:23:48 +0900

    :bug: Fixed a problem that prevented saving amplify_video videos (AVC1 encoding supported).

--- src/twitter_video_dl/twitter_video_dl.py
+++ src/twitter_video_dl/twitter_video_dl.py
@@ -291,7 +291,7 @@ def extract_mp4_fmp4(j):
 
     # pattern looks like https://video.twimg.com/amplify_video/1638969830442237953/vid/1080x1920/lXSFa54mAVp7KHim.mp4?tag=16 or https://video.twimg.com/ext_tw_video/1451958820348080133/pu/vid/720x1280/GddnMJ7KszCQQFvA.mp4?tag=12
     amplitude_pattern = re.compile(
-        r"(https://video.twimg.com/amplify_video/(\d+)/vid/(\d+x\d+)/[^.]+.mp4\?tag=\d+)"
+        r"(https://video.twimg.com/amplify_video/(\d+)/vid/(avc1/)(\d+x\d+)/[^.]+.mp4\?tag=\d+)"
     )
     ext_tw_pattern = re.compile(
         r"(https://video.twimg.com/ext_tw_video/(\d+)/pu/vid/(avc1/)?(\d+x\d+)/[^.]+.mp4\?tag=\d+)"
